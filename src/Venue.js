35c3df97a3504b884ffdb1b1b505b403d41ef18d
Cory Null(Noll) Crimmins - Golden <cory190@live.com>
2018-08-31 21:35:48 -0400

    Added Save and Remove for Venue...

--- src/Venue.js
+++ src/Venue.js
@@ -7,231 +7,239 @@ const Gig = require("./Gig");
 const RESTModel = require("./RESTModel");
 
 class Venue extends RESTModel {
-	get name() {
-		return this.getField("name");
-	}
+  get name() {
+    return this.getField("name");
+  }
 
-	set name(value) {
-		this.setField("name", value);
-	}
+  set name(value) {
+    this.setField("name", value);
+  }
 
-	get description() {
-		return this.getField("description");
-	}
+  get description() {
+    return this.getField("description");
+  }
 
-	set description(value) {
-		this.setField("description", value);
-	}
+  set description(value) {
+    this.setField("description", value);
+  }
 
-	get email() {
-		return this.getField("email");
-	}
+  get email() {
+    return this.getField("email");
+  }
 
-	set email(value) {
-		this.setField("email", value);
-	}
+  set email(value) {
+    this.setField("email", value);
+  }
 
-	get website() {
-		return this.getField("website");
-	}
+  get website() {
+    return this.getField("website");
+  }
 
-	set website(value) {
-		this.setField("website", value);
-	}
+  set website(value) {
+    this.setField("website", value);
+  }
 
-	get phone() {
-		return this.getField("phone");
-	}
+  get phone() {
+    return this.getField("phone");
+  }
 
-	set phone(value) {
-		this.setField("phone", value);
-	}
+  set phone(value) {
+    this.setField("phone", value);
+  }
 
-	get location() {
-		return this.getField("location");
-	}
+  get location() {
+    return this.getField("location");
+  }
 
-	set location(value) {
-		this.setField("location", value);
-	}
+  set location(value) {
+    this.setField("location", value);
+  }
 
-	get openCloseTimes() {
-		return this.getField("openCloseTimes");
-	}
+  get openCloseTimes() {
+    return this.getField("openCloseTimes");
+  }
 
-	set openCloseTimes(value) {
-		this.setField("openCloseTimes", value);
-	}
+  set openCloseTimes(value) {
+    this.setField("openCloseTimes", value);
+  }
 
-	get icon() {
-		return this.getField("icon");
-	}
+  get icon() {
+    return this.getField("icon");
+  }
 
-	set icon(value) {
-		this.setField("icon", value);
-	}
+  set icon(value) {
+    this.setField("icon", value);
+  }
 
-	get photos() {
-		return this.getField("photos");
-	}
+  get photos() {
+    return this.getField("photos");
+  }
 
-	set photos(value) {
-		this.setField("photos", value);
-	}
+  set photos(value) {
+    this.setField("photos", value);
+  }
 
-	get owners() {
-		return this.getField("owners");
-	}
+  get owners() {
+    return this.getField("owners");
+  }
 
-	set owners(value) {
-		this.setField("owners", value);
-	}
+  set owners(value) {
+    this.setField("owners", value);
+  }
 
-	get facebook() {
-		return this.getField("facebook");
-	}
+  get facebook() {
+    return this.getField("facebook");
+  }
 
-	set facebook(value) {
-		this.setField("facebook", value);
-	}
+  set facebook(value) {
+    this.setField("facebook", value);
+  }
 
-	get facebookPageId() {
-		return this.getField("facebookPageId");
-	}
+  get facebookPageId() {
+    return this.getField("facebookPageId");
+  }
 
-	set facebookPageId(value) {
-		this.setField("facebookPageId", value);
-	}
+  set facebookPageId(value) {
+    this.setField("facebookPageId", value);
+  }
 
-	get facebookPageName() {
-		return this.getField("facebookPageName");
-	}
+  get facebookPageName() {
+    return this.getField("facebookPageName");
+  }
 
-	set facebookPageName(value) {
-		this.setField("facebookPageName", value);
-	}
+  set facebookPageName(value) {
+    this.setField("facebookPageName", value);
+  }
 
-	get facebookPageToken() {
-		return this.getField("facebookPageToken");
-	}
+  get facebookPageToken() {
+    return this.getField("facebookPageToken");
+  }
 
-	set facebookPageToken(value) {
-		this.setField("facebookPageToken", value);
-	}
+  set facebookPageToken(value) {
+    this.setField("facebookPageToken", value);
+  }
 
-	get twitter() {
-		return this.getField("twitter");
-	}
+  get twitter() {
+    return this.getField("twitter");
+  }
 
-	set twitter(value) {
-		this.setField("twitter", value);
-	}
+  set twitter(value) {
+    this.setField("twitter", value);
+  }
 
-	get google() {
-		return this.getField("google");
-	}
+  get google() {
+    return this.getField("google");
+  }
 
-	set google(value) {
-		this.setField("google", value);
-	}
+  set google(value) {
+    this.setField("google", value);
+  }
 
-	getIcon(token) {
-		return RESTModel.findById("Upload", this.icon, token, true);
-	}
+  save(token) {
+    return RESTModel.prototype.save.call(this, token, true);
+  }
 
-	getPhotos(token) {
-		const photos = Array.from(this.photos);
-		if (photos.length !== 0)
-			return RESTModel.findMany(
-				"Upload",
-				{
-					_id: photos
-				},
-				token,
-				true
-			);
-		return Promise.resolve([]);
-	}
+  remove(token) {
+    return RESTModel.prototype.remove.call(this, token, true);
+  }
 
-	getOwners(token) {
-		const owners = Array.from(this.owners);
-		if (owners.length !== 0)
-			return RESTModel.findMany(
-				"User",
-				{
-					_id: owners
-				},
-				token,
-				true
-			);
-		return Promise.resolve([]);
-	}
+  getIcon(token) {
+    return RESTModel.findById("Upload", this.icon, token, true);
+  }
 
-	getGigs(token) {
-		const GigClass = Gig || ModelNameToModel("Gig");
-		return GigClass.findByVenue(this._id, token);
-	}
+  getPhotos(token) {
+    const photos = Array.from(this.photos);
+    if (photos.length !== 0)
+      return RESTModel.findMany(
+        "Upload",
+        {
+          _id: photos
+        },
+        token,
+        true
+      );
+    return Promise.resolve([]);
+  }
 
-	getTwitterAccount(token) {
-		return RESTModel.findById("TwitterAccount", this.twitter, token, true);
-	}
+  getOwners(token) {
+    const owners = Array.from(this.owners);
+    if (owners.length !== 0)
+      return RESTModel.findMany(
+        "User",
+        {
+          _id: owners
+        },
+        token,
+        true
+      );
+    return Promise.resolve([]);
+  }
 
-	getFacebookAccount(token) {
-		return RESTModel.findById("FacebookAccount", this.facebook, token, true);
-	}
+  getGigs(token) {
+    const GigClass = Gig || ModelNameToModel("Gig");
+    return GigClass.findByVenue(this._id, token);
+  }
 
-	getLocation(token) {
-		return RESTModel.findById("Location", this.location, token, true);
-	}
+  getTwitterAccount(token) {
+    return RESTModel.findById("TwitterAccount", this.twitter, token, true);
+  }
 
-	valid() {
-		const self = this;
-		if (!super.valid()) return false;
+  getFacebookAccount(token) {
+    return RESTModel.findById("FacebookAccount", this.facebook, token, true);
+  }
 
-		if (!RESTModel.isValidId(this.location)) return false;
-		if (this.location === "") return false;
+  getLocation(token) {
+    return RESTModel.findById("Location", this.location, token, true);
+  }
 
-		if (typeof this.name !== "string") return false;
-		if (this.name === "") return false;
+  valid() {
+    const self = this;
+    if (!super.valid()) return false;
 
-		if (typeof this.description !== "string") return false;
-		if (this.description === "") return false;
-		if (this.description === "<p><br></p>") return false;
+    if (!RESTModel.isValidId(this.location)) return false;
+    if (this.location === "") return false;
 
-		if (!Array.isArray(this.owners)) return false;
-		if (this.owners.length === 0) return false;
-		if (!this.owners.every(owner => self.userIsOwner(owner))) return false;
+    if (typeof this.name !== "string") return false;
+    if (this.name === "") return false;
 
-		return true;
-	}
+    if (typeof this.description !== "string") return false;
+    if (this.description === "") return false;
+    if (this.description === "<p><br></p>") return false;
 
-	// TODO: Create isOpen method
+    if (!Array.isArray(this.owners)) return false;
+    if (this.owners.length === 0) return false;
+    if (!this.owners.every(owner => self.userIsOwner(owner))) return false;
 
-	userIsOwner(user) {
-		if (Array.isArray(this.owners)) {
-			let userId = null;
-			if (typeof user === "string") userId = user;
-			else if (typeof user === "object" && user) userId = user._id;
-			return this.owners.find(id => id === userId) !== undefined;
-		}
-		return false;
-	}
+    return true;
+  }
 
-	static findOne(criteria, token) {
-		return RESTModel.findOne("Venue", criteria, token, true);
-	}
+  // TODO: Create isOpen method
 
-	static findMany(criteria, token) {
-		return RESTModel.findMany("Venue", criteria, token, true);
-	}
+  userIsOwner(user) {
+    if (Array.isArray(this.owners)) {
+      let userId = null;
+      if (typeof user === "string") userId = user;
+      else if (typeof user === "object" && user) userId = user._id;
+      return this.owners.find(id => id === userId) !== undefined;
+    }
+    return false;
+  }
 
-	static findById(id, token) {
-		return RESTModel.findById("Venue", id, token, true);
-	}
+  static findOne(criteria, token) {
+    return RESTModel.findOne("Venue", criteria, token, true);
+  }
 
-	static getAllOwned(token) {
-		return RESTModel.findMany("Venue", null, token, true);
-	}
+  static findMany(criteria, token) {
+    return RESTModel.findMany("Venue", criteria, token, true);
+  }
+
+  static findById(id, token) {
+    return RESTModel.findById("Venue", id, token, true);
+  }
+
+  static getAllOwned(token) {
+    return RESTModel.findMany("Venue", null, token, true);
+  }
 }
 
 Venue.ModelName = "Venue";
